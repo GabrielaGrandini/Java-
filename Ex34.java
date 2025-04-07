36940609db2dba46996d05c87f4ef95eea492246
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-04-07 15:05:18 -0300

    Primeiro programa com Do While

---
+++ Ex34.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+34 - Exemplo de Do while
+
+*******************************************************************************/
+
+public class Ex34
+{
+	public static void main(String[] args) {
+	    int contador = 0;
+		
+		do {
+		    System.out.println("contador");
+		    contador++;
+		} while (contador <= 5);
+	}
+}
