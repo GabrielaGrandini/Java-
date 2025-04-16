e9165e7d9344b4c0f9a74b57094f1ff17beff84b
GabrielaGrandini <gabriela.grandini1a@gmail.com>
2025-04-16 10:43:35 -0300

    Programa que gera números aleatórios e vê qual é maior, menor e sua média

---
+++ Ex51.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+51. Desenvolva um algoritmo que preencha aleatoriamente um vetor de 10 posicões
+com números entre 0 a 100. Calcule e escreva o maior, menor e a média desses números.
+
+===================================================
+Ex.:        import java.util.Random;
+            // Criar um gerador de números aleatórios
+            Random random = new Random();
+===================================================
+
+*******************************************************************************/
+
+import java.util.Random;
+import java.util.Scanner;
+public class Ex51
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner (System.in);
+ 	    Random random = new Random();
+ 	    int[] num =  new int[10];
+ 	    int menor = Integer.MAX_VALUE, maior=0, soma = 0;
+ 	    double  media;
+ 	    
+ 	     System.out.println("Estes são os elementos gerados: ");
+ 	    for(int i = 0; i < 10; i++){
+ 	        num[i] = random.nextInt(100);
+ 	        System.out.print(num[i] + ", ");
+ 	        
+ 	    }
+ 	    
+ 	    System.out.println("  ");
+ 	   
+ 	    
+ 	    
+ 	    for(int i = 0; i < 10; i++){
+    		if (num[i] < menor){
+    		     menor = num[i];
+    		 }
+    		 if(num[i] > maior){
+    		     maior = num[i];
+    		 }
+  
+    		 soma += num[i];
+    	} 
+    	
+		 
+		 media =  soma / num.length;
+		 System.out.println("Esta é a média dos vetores: " + media);
+		 System.out.println("Este é o menor número: " + menor);
+		 System.out.println("Este é o maior número: " + maior);
+ 	    
+	}
+}
